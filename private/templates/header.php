b7e2bd424c9537e6b78e843a32169129a368f222
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-11 10:03:37 +0200

    nav for header.php

---
+++ private/templates/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>InVisual</title>
+</head>
+
+<body>
+    <nav>
+        <div class="nav_links template__nav-item">
+            <span class="logo">InVisual</span>
+        </div>
+
+        <div class="nav_rechts template__nav-item ">
+            <a href="#about">OVER ONS</a>
+            <a href="#projects">PROJECTEN</a>
+            <a href="#shop">SHOP</a>
+            <a href="#contact">CONTACT</a>
+        </div>
+
+        <div class="nav_rechts1">
+            <input type="checkbox" id="nav-toggle" style="display:none;">
+            <label class="nav-toggle-label" for="nav-toggle">
+                <span class="nav-toggle-label-span-1"></span>
+                <span class="nav-toggle-label-span-2"></span>
+                <span class="nav-toggle-label-span-3"></span>
+            </label>
+            <div class="nav-links-container">
+                <a class="nav-link" href="#about">OVER ONS</a>
+                <a class="nav-link" href="#projects">PROJECTEN</a>
+                <a class="nav-link" href="#shop">SHOP</a>
+                <a class="nav-link" href="#contact">CONTACT</a>
+            </div>
+        </div>
+    </nav>
+    <header>
+
+    </header>
