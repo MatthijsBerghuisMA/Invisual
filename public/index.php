91e88908ad458f19c87c2c5a671c7d9f05d67b0f
Wiebe <wieberan@hotmail.nl>
2019-09-11 10:22:36 +0200

    Add a ip tracker

--- public/index.php
+++ public/index.php
@@ -1,5 +1,51 @@
 <?php
 
+// -------------------------------------------------------------------------------
+
+function getUserIpAddr () {
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        // Ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        // Ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+    return $ip;
+}
+
+if (!filter_var(getUserIpAddr(), FILTER_VALIDATE_IP)) {
+    echo "incorrect ip address";
+    die();
+} else {
+    $connected_ip = (string)getUserIpAddr();
+}
+
+// -------------------------------------------------------------------------------
+
+$date = date("Y d m G i s", time());
+
+$new_user = true;
+
+$json_file = "../private/includes/user_data.json";
+
+$obj = json_decode(file_get_contents($json_file, true));
+
+foreach ($obj->recent_ips as $user) {
+    if ($connected_ip == $user->ip) {
+        $new_user = false;
+        $user->last_seen = $date;
+    }
+}
+
+if ($new_user) array_push($obj->recent_ips, (object) array("ip" => "$connected_ip", "last_seen" => $date));
+
+file_put_contents($json_file, json_encode($obj));
+
+// ---------------------------------------------------------------------------------
+
 require "../private/includes/router.php";
 
 $router = new router;
