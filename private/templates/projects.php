2cd55fc6af997f9cad2551374f7092ef89a326a5
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-23 11:27:13 +0200

    re-aligned the file

--- private/templates/projects.php
+++ private/templates/projects.php
@@ -1,33 +1,33 @@
 <div class="projectainer">
-        <section id="projects">
-            <br>
-            <br>
-            <h2>Onze projecten</h2>
-            <br>
-            <div class="pro">
+    <section id="projects">
+        <br>
+        <br>
+        <h2>Onze projecten</h2>
+        <br>
+        <div class="pro">
             <div class="box">
-             <img src="<?= $file_uri ?>img/invisualbla.jpg">
-             <br>
-             <h4>Coming Soon</h4>   
+                <img src="<?= $file_uri ?>img/invisualbla.jpg">
+                <br>
+                <h4>Coming Soon</h4>
                 <br>
             </div>
             <div class="box">
                 <img src="<?= $file_uri ?>img/invisualbla.jpg">
-                <h4>Coming Soon</h4>   
-    
+                <h4>Coming Soon</h4>
+
             </div>
             <div class="box">
                 <img src="<?= $file_uri ?>img/invisualbla.jpg">
                 <br>
-                <h4>Coming Soon</h4>   
+                <h4>Coming Soon</h4>
             </div>
             <div class="box">
-                    <img src="<?= $file_uri ?>img/invisualbla.jpg">
-                    <br>
-                    <h4>Coming Soon</h4>   
+                <img src="<?= $file_uri ?>img/invisualbla.jpg">
+                <br>
+                <h4>Coming Soon</h4>
             </div>
         </div>
         <br>
         <br>
-        </section>
-        </div>
+    </section>
+</div>
