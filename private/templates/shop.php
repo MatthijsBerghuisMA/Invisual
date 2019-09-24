1eaf20043b2cc315bab8280df6412eacdeedbada
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-24 14:37:16 +0200

    created shop.php

---
+++ private/templates/shop.php
@@ -0,0 +1,35 @@
+<!-- Made by Matthijs -->
+
+<?= var_dump($file_uri) ?>
+<div class="bannershop"></div>
+<div class="container">
+    <div class="shop-text">
+        <span class="head-span">Onze pakketten</span>
+        <span class="lower-span">Wij zijn gespecialiseerd in het ontwerpen <br>
+van logo’s, huisstijlen en websites</span>
+    </div>
+    <div class="shop">
+        <div class="shop-block">
+            <span class="shop-item">logo</span>
+            <span class="shop-prijs">€89</span>
+            <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 708.66 708.66"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#48a9ca;}</style></defs><title>pakketiconen</title><circle class="cls-1" cx="240.41" cy="359.43" r="207.85"/><path class="cls-2" d="M240.41,165.63c107,0,193.8,86.77,193.8,193.8s-86.77,193.79-193.8,193.79S46.62,466.46,46.62,359.43s86.76-193.8,193.79-193.8m0-28.11A221.34,221.34,0,1,0,326.8,155a220.5,220.5,0,0,0-86.39-17.45Z"/><polygon class="cls-1" points="331.53 613.49 234.54 445.5 331.53 277.51 525.5 277.51 622.5 445.5 525.5 613.49 331.53 613.49"/><path class="cls-2" d="M517.39,291.57,606.26,445.5,517.39,599.43H339.64L250.77,445.5l88.87-153.93H517.39m16.23-28.12H323.41l-8.12,14.06L226.42,431.44,218.3,445.5l8.12,14.06,88.87,153.93,8.12,14.06H533.62l8.12-14.06,88.87-153.93,8.12-14.06-8.12-14.06L541.74,277.51l-8.12-14.06Z"/><path class="cls-1" d="M381.12,408.76a16.73,16.73,0,0,1-13.18-6.51,16.54,16.54,0,0,1-2.86-14.41l22.35-83.39L597.81,94.07l7.26,1.33a90.57,90.57,0,0,1,72.8,72.81l1.33,7.25L468.82,385.84l-83.39,22.35a16.57,16.57,0,0,1-4.31.57Z"/><path class="cls-2" d="M602.54,109.23A76.5,76.5,0,0,1,664,170.74l-202.5,202.5h0l-79.75,21.37a2.63,2.63,0,0,1-.67.09,2.57,2.57,0,0,1-2.46-3.22L400,311.73l202.51-202.5m-9.46-30.31L582.65,89.35l-202.5,202.5-5.33,5.33-1.95,7.27L351.5,384.21a30.49,30.49,0,0,0,5.29,26.6,30.64,30.64,0,0,0,32.28,11l79.75-21.37,7.27-1.95,5.33-5.33,202.51-202.5,10.42-10.43-2.65-14.51a104.63,104.63,0,0,0-84.11-84.11l-14.51-2.65Z"/></svg>
+            <span class="item-info">5 voorstellen logo</span>
+            <a href="#" class="shop-button">kiezen</a>
+        </div>
+        <div class="shop-block">
+        <span class="shop-item">huisstijl</span>
+            <span class="shop-prijs">€599</span>
+            <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 708.66 708.66"><defs><style>.cls-1,.cls-3{fill:#fff;}.cls-2{fill:#48a9ca;}.cls-3{stroke:#48a9ca;stroke-linecap:round;stroke-miterlimit:10;stroke-width:28.12px;}</style></defs><title>pakketiconen</title><rect class="cls-1" x="78.3" y="94.97" width="368.24" height="486.75"/><path class="cls-2" d="M432.49,109V567.66H92.36V109H432.49M460.6,80.91H64.25V595.78H460.6V80.91Z"/><rect class="cls-1" x="260.99" y="398.27" width="369.37" height="215.43"/><path class="cls-2" d="M616.3,412.33V599.64H275.05V412.33H616.3m28.12-28.12H246.93V627.75H644.42V384.21Z"/><polygon class="cls-1" points="438.91 518.3 268.29 424.65 275.05 398.27 616.3 398.27 623.06 424.65 452.44 518.3 438.91 518.3"/><path class="cls-2" d="M616.3,412.33h0m0,0L445.67,506,275.05,412.33H616.3m0-28.12H275.05L261.52,437l170.63,93.66H459.2L629.83,437,616.3,384.23v0Zm0,56.23Z"/><rect class="cls-1" x="349.38" y="176.77" width="268.09" height="164"/><path class="cls-2" d="M603.41,190.83V326.72h-240V190.83h240m28.12-28.12H335.32V354.83H631.53V162.71Z"/><line class="cls-3" x1="519.34" y1="233.08" x2="567.94" y2="233.08"/><line class="cls-3" x1="475.79" y1="273.9" x2="567.94" y2="273.9"/><line class="cls-3" x1="137.99" y1="180.28" x2="293.74" y2="180.28"/><line class="cls-3" x1="137.99" y1="239.28" x2="236.98" y2="239.28"/><line class="cls-3" x1="137.99" y1="289.38" x2="293.74" y2="289.38"/><line class="cls-3" x1="137.99" y1="339.47" x2="293.74" y2="339.47"/><line class="cls-3" x1="137.99" y1="390.33" x2="210.84" y2="390.33"/><line class="cls-3" x1="137.99" y1="441.19" x2="210.84" y2="441.19"/><line class="cls-3" x1="137.99" y1="492.05" x2="210.84" y2="492.05"/></svg>            
+            <span class="item-info">5 voorstellen <br> logo, visitekaartje <br> en briefpapier</span>
+            <a href="#" class="shop-button">kiezen</a>
+        </div>
+        <div class="shop-block">
+        <span class="shop-item">website</span>
+            <span class="shop-prijs">€1049</span>
+            <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 708.66 708.66"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#48a9ca;}</style></defs><title>website_icoon</title><rect class="cls-1" x="33.52" y="93.14" width="641.62" height="401.79"/><path class="cls-2" d="M659.26,109v370H49.4V109H659.26M691,77.26H17.64V510.81H691V77.26Z"/><rect class="cls-1" x="173.71" y="168.73" width="361.24" height="57.03"/><path class="cls-2" d="M519.07,184.61v25.27H189.59V184.61H519.07M537,152.85H171.71a13.87,13.87,0,0,0-13.88,13.88v61a13.88,13.88,0,0,0,13.88,13.88H537a13.88,13.88,0,0,0,13.88-13.88v-61A13.87,13.87,0,0,0,537,152.85Z"/><rect class="cls-1" x="173.71" y="280.5" width="105.62" height="138.85"/><path class="cls-2" d="M263.45,296.38V403.47H189.59V296.38h73.86m17.87-31.76H171.71a13.87,13.87,0,0,0-13.88,13.88V421.35a13.88,13.88,0,0,0,13.88,13.88H281.32a13.89,13.89,0,0,0,13.89-13.88V278.5a13.88,13.88,0,0,0-13.89-13.88Z"/><rect class="cls-1" x="336.71" y="280.5" width="198.24" height="138.85" rx="2.08"/><path class="cls-2" d="M519.07,296.38V403.47H352.59V296.38H519.07m13.8-31.76H338.79a18,18,0,0,0-18,18V417.27a18,18,0,0,0,18,18H532.87a18,18,0,0,0,18-18V282.58a18,18,0,0,0-18-18Z"/><polygon class="cls-1" points="249.72 617.52 294.39 553.91 273.73 495.22 434.99 495.22 414.33 553.91 459 617.52 249.72 617.52"/><path class="cls-2" d="M412.56,511.1q-7.95,22.62-15.92,45.24l31.8,45.3H280.28l31.8-45.3q-8-22.62-15.92-45.24h116.4m44.85-31.76H251.31l14.89,42.3,8,22.63,2.53,7.2-6.51,9.27-15.9,22.65-35.11,50H489.55l-35.12-50-15.9-22.65L432,551.47l2.54-7.2,8-22.63,14.89-42.3Zm-44.85,63.52h0Z"/></svg>
+            <span class="item-info">Geheel complete <br> website</span>
+            <a href="#" class="shop-button">kiezen</a>
+        </div>
+
+    </div>
+</div>
