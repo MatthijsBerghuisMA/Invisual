15dfc32e67bb34ab9c45a2296c98bec765ef7108
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-25 11:32:56 +0200

    small changes
    i've made some small changes to the php files

--- private/templates/header.php
+++ private/templates/header.php
@@ -9,11 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?=$file_uri?>css/style.css">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Montserrat:800&display=swap" rel="stylesheet"> -->
     <title>Invisual<?php if ($main_route != "home") echo " | " . $main_route;?></title>
 </head>
-    <style>
-    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
-    </style>
 <body>
     <nav>
         <div class="nav_links template__nav-item">
@@ -21,7 +19,7 @@
         </div>
 
         <div class="nav_rechts template__nav-item ">
-            <a class="nav-link" href="<?=$nav_uri?>about">over ons</a>
+            <a class="nav-link" href="/">home</a>
             <a class="nav-link" href="<?=$nav_uri?>projects">projecten</a>
             <a class="nav-link" href="<?=$nav_uri?>shop">diensten</a>
             <a class="nav-link" href="<?=$nav_uri?>contact">contact</a>
@@ -35,7 +33,7 @@
                 <span class="nav-toggle-label-span-3"></span>
             </label>
             <div class="nav-links-container">
-            <a class="nav-link" href="<?=$nav_uri?>about">over ons</a>
+            <a class="nav-link" href="/">home</a>
             <a class="nav-link" href="<?=$nav_uri?>projects">projecten</a>
             <a class="nav-link" href="<?=$nav_uri?>shop">diensten</a>
             <a class="nav-link" href="<?=$nav_uri?>contact">contact</a>
--- private/templates/home.php
+++ private/templates/home.php
@@ -6,7 +6,7 @@
         <div class="test"></div>
         <!-- <img class="test" src="<?= $file_uri ?>img/banner.jpg" alt="banner1"> -->
         <div class="slogan">
-            <span class="headsl">WIJ VISUASEREN JOUW VISIE!</span>
+            <span class="headsl">Wij visualiseren <br> jouw visie!</span>
             <span class="subslogen">Opzoek naar een passende huisstijl voor jouw bedrijf? <br>
                 Dan ben je bij ons aan het juiste adres! <br>
                 Wij helpen jou graag aan je nieuwe huisstijl.
@@ -15,14 +15,14 @@
     </div>
 
     <div class="logoN">
-        <span>LOGO NODIG?</span>
-        <span>Wij denken graag met jouw mee</span>
+        <span class="fontb">LOGO NODIG?</span>
+        <span>Wij denken graag <br> met jouw mee</span>
         <img src="<?= $file_uri ?>img/foto.png" alt="test">
         <a class="button-buy" href="#">DIT WIL IK</a>
     </div>
 
     <div class="websiteNodig">
-        <span>Website Nodig?</span>
+        <span class="fontb">Website Nodig?</span>
         <span>Wij zijn er voor jou</span>
         <img src="<?= $file_uri ?>img/10.jpg" alt="test">
         <a class="button-buy" href="#">DIT WIL IK</a>
