8e2960c534592c705396df703bdf37758c5b6f81
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-26 16:29:01 +0200

    created "ons team"

--- private/templates/home.php
+++ private/templates/home.php
@@ -27,6 +27,51 @@
         <img src="<?= $file_uri ?>img/10.jpg" alt="test">
         <a class="button-buy" href="shop">DIT WIL IK</a>
     </div>
+    <span class="meet" >Ontmoet ons team!</span>
+    <div class="team">
+ 
+        <div class="team-blokken">
+            <div class="teamblok">
+                <img class="person" src="<?= $file_uri ?>img/unknown.png" alt="img">
+                <span class="naam">Matthijs Berghuis</span>
+                <span class="functie">Web-developer</span>
+                <a href="https://matthijsberghuis.com" target="_blank" class="button-buy">portoflio</a>
+            </div>
 
+            <div class="teamblok">
+                <img class="person" src="<?= $file_uri ?>img/unknown.png" alt="img">
+                <span class="naam">Laura Bruin</span>
+                <span class="functie">Vormgever</span>
+                <a href="https://laurabruin.nl/" target="_blank" class="button-buy">portoflio</a>
+            </div>
 
+            <div class="teamblok">
+                <img class="person" src="<?= $file_uri ?>img/unknown.png" alt="img">
+                <span class="naam">Wiebe Ranzijn</span>
+                <span class="functie">Fulltime Hentai Dokter</span>
+                <a href="http://wieberanzijn.com/" target="_blank" class="button-buy">portoflio</a>
+            </div>
+            
+            <div class="teamblok">
+                <img class="person" src="<?= $file_uri ?>img/unknown.png" alt="img">
+                <span class="naam">Yeno Reus</span>
+                <span class="functie">Web-developer</span>
+                <a href="https://yenoreus.nl/" target="_blank" class="button-buy">portoflio</a>
+            </div>
+            
+            <div class="teamblok">
+                <img class="person" src="<?= $file_uri ?>img/unknown.png" alt="img">
+                <span class="naam">Richi Franken</span>
+                <span class="functie">Vormgever</span>
+                <a href="https://richifranken.nl/" target="_blank" class="button-buy">portoflio</a>
+            </div>
+            
+            <div class="teamblok">
+                <img class="person" src="<?= $file_uri ?>img/unknown.png" alt="img">
+                <span class="naam">Sam Harwig</span>
+                <span class="functie">Web-Developer</span>
+                <a href="http://samharwig.nl/" target="_blank" class="button-buy">portoflio</a>
+            </div>
+        </div>
+    </div>
 </div>
