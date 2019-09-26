8161daaa14ba64e6e4ced3590f477aae6bdf31e7
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-26 23:39:32 +0200

    small changes

--- private/templates/projects.php
+++ private/templates/projects.php
@@ -7,10 +7,10 @@
         </div>
         <div class="projecten">
             <div class="projectbig">
-            <img src="<?=$file_uri?>img/matt.png" class="ButtonM" alt="?">
+                <img src="<?= $file_uri ?>img/matt.png" class="ButtonM" alt="?">
                 <section class="album Content">
                     <h1>projectname</h1>
-                    <img src="<?=$file_uri?>img/mattmodaal.png" alt="?">
+                    <img src="<?= $file_uri ?>img/mattmodaal.png" alt="?">
                     <article>
                         <a href="http://matthijsberghuis.com">Link</a>
                         <!-- desnoods een stukje text -->
@@ -20,45 +20,59 @@
 
             <div class="smallsquare">
                 <div class="projectsmall">
-                <img src="<?=$file_uri?>img/download.png" class="ButtonM" alt="?">
-                <section class="album Content">
-                    <h1>projectname</h1>
-                    <img src="<?=$file_uri?>img/11.jpg" alt="?">
-                    <article>
-                        <a href="http://">Link</a>
-                        <!-- desnoods een stukje text -->
-                    </article>
-                </section>
+                    <img src="<?= $file_uri ?>img/download.png" class="ButtonM" alt="?">
+                    <!-- img outside view needs to be square -->
+                    <section class="album Content">
+                        <h1>projectname</h1>
+                        <img src="<?= $file_uri ?>img/11.jpg" alt="?">
+                        <!-- img inside view can be whatever you want-->
+                        <article>
+                            <a href="http://">Link</a>
+                            <!-- website url -->
+                            <!-- piece of txt -->
+                        </article>
+                    </section>
                 </div>
                 <div class="projectsmall">
-                <img src="<?=$file_uri?>img/download.png" class="ButtonM" alt="?">
+                    <img src="<?= $file_uri ?>img/download.png" class="ButtonM" alt="?">
+                    <!-- img outside view needs to be square -->
+                    <section class="album Content">
+                        <h1>projectname</h1>
+                        <img src="<?= $file_uri ?>img/11.jpg" alt="?">
+                        <!-- img inside view can be whatever you want-->
+                        <article>
+                            <a href="http://">Link</a>
+                            <!-- website url -->
+                            <!-- piece of txt -->
+                        </article>
+                    </section>
+                </div>
+                <div class="projectsmall"> <img src="<?= $file_uri ?>img/download.png" class="ButtonM" alt="?">
+                    <!-- img outside view needs to be square -->
+                    <section class="album Content">
+                        <h1>projectname</h1>
+                        <img src="<?= $file_uri ?>img/11.jpg" alt="?">
+                        <!-- img inside view can be whatever you want-->
+                        <article>
+                            <a href="http://">Link</a>
+                            <!-- website url -->
+                            <!-- piece of txt -->
+                        </article>
+                    </section>
+                </div>
+                <div class="projectsmall">    <img src="<?=$file_uri?>img/download.png" class="ButtonM" alt="?">
+                <!-- img outside view needs to be square -->
                 <section class="album Content">
                     <h1>projectname</h1>
                     <img src="<?=$file_uri?>img/11.jpg" alt="?">
+                    <!-- img inside view can be whatever you want-->
                     <article>
                         <a href="http://">Link</a>
-                        <!-- desnoods een stukje text -->
+                    <!-- website url -->
+                        <!-- piece of txt -->
                     </article>
                 </section>
                 </div>
-                <div class="projectsmall"><img src="<?=$file_uri?>img/download.png" class="ButtonM" alt="?">
-                <section class="album Content">
-                    <h1>projectname</h1>
-                    <img src="<?=$file_uri?>img/11.jpg" alt="?">
-                    <article>
-                        <a href="http://">Link</a>
-                        <!-- desnoods een stukje text -->
-                    </article>
-                </section></div>
-                <div class="projectsmall"><img src="<?=$file_uri?>img/download.png" class="ButtonM" alt="?">
-                <section class="album Content">
-                    <h1>projectname</h1>
-                    <img src="<?=$file_uri?>img/11.jpg" alt="?">
-                    <article>
-                        <a href="http://">Link</a>
-                        <!-- desnoods een stukje text -->
-                    </article>
-                </section></div>
             </div>
             <div class="smallsquare">
                 <div class="projectsmall"></div>
